57dcb79856f141d25cf16f1b752f07e0973b1f2a
Akshat-Das <75249561+Akshat-Das@users.noreply.github.com>
2022-07-01 10:57:49 +0530

    Time: 42 ms (94.62%), Space: 17 MB (34.85%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(),boxTypes.end(),cmp);
+        int ans=0;
+        for(auto box: boxTypes){
+            int x=min(box[0],truckSize);  
+            ans+=(x*box[1]);  
+            truckSize-=x;
+            if(truckSize <= 0) break;  //capacity full
+        }
+        return ans;
+    }
+    static bool cmp(vector<int>&a,vector<int>&b){
+        return a[1] > b[1];
+    }
+};
