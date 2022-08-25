0a110ab5ecfee35c3268f786c1ca079ffa9bba58
Akshat-Das <75249561+Akshat-Das@users.noreply.github.com>
2022-08-25 12:34:31 +0530

    Time: 12 ms (91.71%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int a[26]={0},b[26]={0};
-        bool answer = false;
+        bool answer = true;
         for(auto i :ransomNote)
             a[i-'a']++;
         for(auto j : magazine)
             b[j - 'a']++;
         for(int i=0;i<26;i++){
             if(a[i] <= b[i]){
-                answer = true;
             }else{
                 answer = false;
                 return answer;
