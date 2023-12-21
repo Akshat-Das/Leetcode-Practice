073ff62b360f4c0185f5b1abc7292f4d4b788a65
Akshat-Das <75249561+Akshat-Das@users.noreply.github.com>
2023-12-22 03:17:25 +0530

    Time: 18 ms (57.47%) | Memory: 18.3 MB (79.16%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int arr[9] = {0};
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] != '.'){
+                    if(board[i][j] > '9' || board[i][j] < '1'){
+                        //cout << "Hi";
+                        return false;
+                    }
+                    arr[board[i][j] - '1'] += 1;
+                    // cout << board[i][j]<< " ";
+                }
+            }
+            for(int k=0;k<9;k++) if(arr[k] > 1){
+                        //cout << arr[k] << " " << k;
+                        //cout << "Hi2";
+                        return false;
+            }
+            for(int k=0;k<9;k++) arr[k] = 0;
+        }
+        for(int j=0;j<9;j++){
+            for(int i=0;i<9;i++){
+                if(board[i][j] != '.'){
+                    if(board[i][j] > '9' || board[i][j] < '1'){
+                        //cout << "Hi";
+                        return false;
+                    }
+                    arr[board[i][j] - '1'] += 1;
+                    // cout << board[i][j]<< " ";
+                }
+            }
+            for(int k=0;k<9;k++) if(arr[k] > 1){
+                        //cout << arr[k] << " " << k;
+                        //cout << "Hi2";
+                        return false;
+            }
+            for(int k=0;k<9;k++) arr[k] = 0;
+        }
+        
+        for(int s = 0;s<3;s++){
+            for(int i=3*s;i < (3*s) + 3;i++){
+                for(int j=0;j<3;j++){
+                    if(board[i][j] != '.'){
+                        if(board[i][j] > '9' || board[i][j] < '1'){
+                            return false;
+                        }
+                        arr[board[i][j] - '1'] += 1;
+                    }
+                }
+            }
+            for(int k=0;k<9;k++) if(arr[k] > 1)return false;
+            for(int k=0;k<9;k++) arr[k] = 0;
+        }
+
+        for(int s = 0;s<3;s++){
+            for(int i=3*s;i < (3*s) + 3;i++){
+                for(int j=3;j<6;j++){
+                    if(board[i][j] != '.'){
+                        if(board[i][j] > '9' || board[i][j] < '1'){
+                            return false;
+                        }
+                        arr[board[i][j] - '1'] += 1;
+                    }
+                }
+            }
+            for(int k=0;k<9;k++) if(arr[k] > 1)return false;
+            for(int k=0;k<9;k++) arr[k] = 0;
+        }
+
+        for(int s = 0;s<3;s++){
+            for(int i=3*s;i < (3*s) + 3;i++){
+                for(int j=6;j<9;j++){
+                    if(board[i][j] != '.'){
+                        if(board[i][j] > '9' || board[i][j] < '1'){
+                            return false;
+                        }
+                        arr[board[i][j] - '1'] += 1;
+                    }
+                }
+            }
+            for(int k=0;k<9;k++) if(arr[k] > 1)return false;
+            for(int k=0;k<9;k++) arr[k] = 0;
+        }
+        return true;
+    }
+};
+
